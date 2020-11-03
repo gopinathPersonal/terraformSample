72ba05c81eb22d00fa7d4b77c793c32eb2a7bb39
gopinathPersonal <63572096+gopinathPersonal@users.noreply.github.com>
2020-11-02 22:51:01 -0800

    Create variable.tf

---
+++ vpc/variable.tf
@@ -0,0 +1,25 @@
+variable "region" {
+  default = ""
+}
+
+variable "vpc_cidr" {
+  default = ""
+}
+
+variable "subnet01_cidr" {
+  default = ""
+}
+
+variable "subnet02_cidr" {
+  default = ""
+}
+
+variable "ingress_22" {
+
+}
+variable "ingress_80" {
+
+}
+
+variable "ingress_3389" {
+  }

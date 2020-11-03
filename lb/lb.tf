7d3fc775e40485f88051b823288ac5f2e14bd576
gopinathPersonal <63572096+gopinathPersonal@users.noreply.github.com>
2020-11-02 22:54:16 -0800

    Create lb.tf

---
+++ lb/lb.tf
@@ -0,0 +1,35 @@
+# Create a new load balancer
+resource "aws_elb" "Gopi_lb" {
+  name               = "Gopi-lb-01"
+  subnets                = var.subnets
+  security_groups        = [var.security_groups]
+
+listener {
+    instance_port     = 80
+    instance_protocol = "http"
+    lb_port           = 80
+    lb_protocol       = "http"
+  }
+
+  health_check {
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    timeout             = 3
+    target              = "HTTP:80/"
+    interval            = 30
+  }
+
+  cross_zone_load_balancing   = true
+  idle_timeout                = 400
+  connection_draining         = true
+  connection_draining_timeout = 400
+
+  tags = {
+    Name = "Gopi_lb"
+  }
+}
+
+
+output "LB" {
+  value = aws_elb.Gopi_lb.id
+}

30a6c09ad9a69ffde7a3e655d73a6901051baffc
gopinathPersonal <63572096+gopinathPersonal@users.noreply.github.com>
2020-11-02 22:53:07 -0800

    Create variable.tf

---
+++ ec2/variable.tf
@@ -0,0 +1,23 @@
+variable "ami_ec2" {
+  default = ""
+}
+
+variable "instance_type_ec2" {
+  default = ""
+}
+
+variable "key_name_ec2" {
+  default = ""
+}
+
+variable "subnet01" {
+  default = ""
+}
+
+variable "security_groups" {
+
+}
+
+variable "elb" {
+default = ""
+}

09ee140894c5d361c82ceb3c8d2e6b08933a5f66
gopinathPersonal <63572096+gopinathPersonal@users.noreply.github.com>
2020-11-02 22:47:23 -0800

    Variable file

---
+++ variable.tf
@@ -0,0 +1,39 @@
+variable "region" {
+  default = "us-east-2"
+}
+
+variable "vpc_cidr" {
+  default = "13.0.0.0/16"
+}
+
+variable "subnet01_cidr" {
+  default = "13.0.1.0/24"
+}
+
+variable "subnet02_cidr" {
+  default = "13.0.2.0/24"
+}
+
+variable "ingress_22" {
+  default = 22
+}
+
+variable "ingress_80" {
+  default = 80
+}
+
+variable "ingress_3389" {
+  default = 3389
+}
+
+variable "ami_ec2" {
+  default = "ami-0528a5175983e7f28"
+}
+
+variable "instance_type_ec2" {
+  default = "t2.micro"
+}
+
+variable "key_name_ec2" {
+  default = "oregonkeypair"
+}

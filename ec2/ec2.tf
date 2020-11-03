fe5173d995bfbb6c846b10889dfb3b2eb4794fcc
gopinathPersonal <63572096+gopinathPersonal@users.noreply.github.com>
2020-11-02 22:52:33 -0800

    Create ec2.tf

---
+++ ec2/ec2.tf
@@ -0,0 +1,24 @@
+# Ec2 Instance Creation
+
+resource "aws_instance" "Gopi_server" {
+  ami           = var.ami_ec2
+  instance_type = var.instance_type_ec2
+  key_name = var.key_name_ec2
+  subnet_id = var.subnet01
+  security_groups  = [var.security_groups]
+
+  tags = {
+    Name = "Gopi_server"
+  }
+}
+
+# Adding Instance as a Backend Server of LB
+resource "aws_elb_attachment" "Hitec_addec2" {
+  elb      = var.elb
+  instance = aws_instance.Gopi_server.id
+}
+
+# output
+output "instance" {
+  value = aws_instance.Gopi_server.id
+}

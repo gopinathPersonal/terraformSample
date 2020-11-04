519698eb35d8d24a7320b5814e0854040f42861f
gopinathPersonal <63572096+gopinathPersonal@users.noreply.github.com>
2020-11-03 18:35:52 -0800

    Create jenkinspipeline.groovy

---
+++ jenkinspipeline.groovy
@@ -0,0 +1,47 @@
+pipeline {
+	agent {label 'master'}
+	
+	stages {
+		stage('CLEAN WORKSPACE IN JENKINS SERVER'){
+			steps {
+				cleanWs()
+			} // Steps Completed	
+		}  // Stage Completed
+	
+
+		stage('CLONE THE SOURCE CODE FROM GIT-HUB'){
+			steps {
+				echo 'In SCM Stage'
+				
+				git credentialsId: 'f278eddc-9430-40ea-8dd6-048a31ddca83', url: 'https://github.com/gopinathPersonal/terraformSample.git',branch: 'main'
+				sh '''
+					ls
+					pwd
+				'''
+
+			} // Steps Completed
+		}  // Stage Completed	
+
+
+		stage('initiating terraform'){
+			steps {
+				
+				sh'''
+					terraform init
+				'''
+			} // Steps Completed
+		}  // Stage Completed
+
+		stage('Applying terraform'){
+			steps {
+				
+				sh'''
+					terraform apply
+				'''
+			} // Steps Completed
+		}  // Stage Completed
+
+
+		
+	}
+}
